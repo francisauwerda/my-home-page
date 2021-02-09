cf85f69a2ef7a43e06985169bab2cb45cbc76933
Francis Auwerda <francisauwerda@Franciss-MBP.lan>
2021-02-09 16:36:39 +0100

    Basic home page for my website

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,68 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "../styles/Home.module.css";
+
+function ImageLink({ children, href }) {
+  return (
+    <Link passHref={true} href={href}>
+      <a target="_blank" className={styles.imageLink}>
+        {children}
+      </a>
+    </Link>
+  );
+}
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Francis Auwerda</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="Description"
+          content="Home page for reaching Francis Auwerda"
+        />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <section className={styles.section}>
+        <main>
+          <div className={styles.titleContainer}>
+            <div className={styles.profilePictureContainer}>
+              <Image
+                src="/images/profile.png"
+                alt="Profile photo of Francis"
+                width="100%"
+                height="100%"
+                className="profile-picture"
+              />
+            </div>
+            <h1 className={styles.title}>Francis Auwerda</h1>
+          </div>
+          <p className={styles.description}>
+            Hello and welcome 👋
+            <br />
+            Click on the links if you would like to reach me
+          </p>
+        </main>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <aside className={styles.aside}>
+          <ImageLink href="https://github.com/francisauwerda">
+            <div className={styles.githubContainer}>
+              <img src="/images/github.png" alt="GitHub" width={"100%"} />
+            </div>
+          </ImageLink>
+          <ImageLink href="https://www.linkedin.com/in/francis-auwerda">
+            <div className={styles.linkedInContainer}>
+              <img src="/images/linkedin.png" alt="LinkedIn" width={"100%"} />
+            </div>
+          </ImageLink>
+        </aside>
+      </section>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      {/* <article className={styles.article}>✌️</article> */}
     </div>
-  )
+  );
 }
